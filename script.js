7370dcfa4eca89a1db74b5e233cf1c9730f122a5
Ralph.Vitug <rralph@rrc.ca>
2025-09-30 09:22:31 -0500

    Added variables that manipulates HTML

--- script.js
+++ script.js
@@ -2,3 +2,20 @@
 let lives = 3;
 let position = 0;
 const finish = 10;
+
+const livesE1 = document.getElementById("lives");
+const positionE1 = document.getElementById("position");
+const rollBtn = document.getElementById("rollBtn");
+const continueBtn = document.getElementById("continueBtn");
+const message = document.getElementById("message");
+
+
+
+// Q @ A portion
+const questions = [
+  {q: "What color is Mars known as?", options: ["Red planet", "Blue planer", "Planet of the Apes"], answer: "Red planet"},
+  { q: "How many moons does Mars have?", options: ["1","2","3","4"], answer: "2" },
+  { q: "Largest volcano on Mars?", options: ["Mauna Kea","Mount Everest","Olympus Mons","Vesuvius"], answer: "Olympus Mons" },
+  { q: "What gas makes up most of Mars' atmosphere?", options: ["Oxygen","Carbon Dioxide","Nitrogen","Methane"], answer: "Carbon Dioxide" },
+  { q: "Which rover landed in 2021?", options: ["Spirit","Curiosity","Opportunity","Perseverance"], answer: "Perseverance" }
+]
